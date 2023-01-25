d1dde348b1dc70050ac7c0512a911e5618d46976
kwesi-codekid <107305649+kwesi-codekid@users.noreply.github.com>
2023-01-25 18:09:32 +0000

    Frontend setup

---
+++ src/pages/Start.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Start = () => {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen bg-[#03094a] gap-[2rem]'>
+            <h2 className='font-bold text-4xl text-white text-center'>
+                Verify Document
+            </h2>
+            <button className='py-2 w-32 rounded-full bg-red-700 text-slate-100 text-xl font-bold'>
+                Start
+            </button>
+        </div>
+    )
+}
+
+export default Start
